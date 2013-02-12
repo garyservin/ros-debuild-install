0db70055578e8fb27463cd02925eec1c0c27f0b0
Michael Janssen <jamuraa@base0.net>
2013-02-11 18:19:09 -0600

    Be a little louder for debugging

--- ros_gbp_build_debians.py
+++ ros_gbp_build_debians.py
@@ -58,13 +58,13 @@ class VcsPackageFetcher(object):
       if not updated:
         print("WARNING: Repo at %s changed url from %s to %s or update failed. Redownloading!" % (repo_path, client.get_url(), repo_url))
         shutil.rmtree(repo_path)
+        checkedout = client.checkout(pkg_info['url'], pkg_info['version'], shallow=True, verbose=True)
         client._do_fetch()
-        checkedout = client.checkout(pkg_info['url'], pkg_info['version'], shallow=False)
         if not checkedout:
           print("ERROR: Repo at %s could not be checked out from %s with version %s!" % (repo_path, repo_url, pkg_info['version']))
 
     else:
-      checkedout = client.checkout(pkg_info['url'], pkg_info['version'], shallow=False)
+      checkedout = client.checkout(pkg_info['url'], pkg_info['version'], shallow=True, verbose=True)
       if not checkedout:
         print("ERROR: Repo at %s could not be checked out from %s with version %s!" % (repo_path, repo_url, pkg_info['version']))
 
