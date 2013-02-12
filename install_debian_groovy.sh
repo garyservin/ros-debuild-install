b832bc0a105fa66f84d69f7cafcb12b262635b3a
Michael Janssen <jamuraa@base0.net>
2013-02-12 04:47:21 -0600

    bump rosinstall version

--- install_debian_groovy.sh
+++ install_debian_groovy.sh
@@ -100,7 +100,7 @@ BASE_PACKAGES_URL="http://packages.ros.org/ros/ubuntu/"
 # NOTE: order is important.
 ros_bootstrap_package python-catkin-pkg 0.1.9 "${BASE_PACKAGES_URL}pool/main/c/catkinpkg/python-catkin-pkg_0.1.9-1_all.deb"
 ros_bootstrap_package python-vcstools   0.1.28 "${BASE_PACKAGES_URL}pool/main/v/vcstools/python-vcstools_0.1.28-1_all.deb"
-ros_bootstrap_package python-rosinstall 0.6.23 "${BASE_PACKAGES_URL}pool/main/r/rosinstall/python-rosinstall_0.6.23-1_all.deb"
+ros_bootstrap_package python-rosinstall 0.6.24 "${BASE_PACKAGES_URL}pool/main/r/rosinstall/python-rosinstall_0.6.24-1_all.deb"
 ros_bootstrap_package python-rospkg     1.0.18 "${BASE_PACKAGES_URL}pool/main/r/rospkg/python-rospkg_1.0.18-1_all.deb"
 ros_bootstrap_package python-rosdep     0.10.13 "${BASE_PACKAGES_URL}pool/main/r/rosdep/python-rosdep_0.10.13-1_all.deb"
 ros_bootstrap_package python-wstool     0.0.2 "${BASE_PACKAGES_URL}pool/main/w/wstool/python-wstool_0.0.2-1_all.deb"
